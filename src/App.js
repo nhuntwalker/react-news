b464a76e82be2935e2a50d60062dfb9cd04f0481
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-03-24 06:14:20 -0700

    Learned new shorthand for ES6 objects where if we want to use a key name that shares a name with the object we want to bind to it, we could just use the key name on its own

--- src/App.js
+++ src/App.js
@@ -21,10 +21,15 @@ const list = [
 ]
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {list,};
+  }
+
   render() {
     return (
       <div className="App">
-        { list.map(item => 
+        { this.state.list.map(item => 
           <div key={ item.objectID }>
             <span><a href={ item.url }>{ item.title }</a></span>
             <span>{ item.author }</span>
