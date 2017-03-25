9afb98e48761175739b293278f147bd9c45be7bc
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-03-25 12:56:18 -0700

    Learned how to use the fetch function along with the component life cycle to retrieve and handle data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-]
+const DEFAULT_QUERY = 'redux',
+      PATH_BASE = 'https://hn.algolia.com/api/v1',
+      PATH_SEARCH = '/search',
+      PARAM_SEARCH = 'query=';
+
+const URL = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 const isSearched = (searchTerm) => (item) => !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -26,11 +14,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  componentDidMount(){
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  fetchSearchTopStories(searchTerm){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result));
+  }
+
+  setSearchTopStories(result){
+    this.setState({ result });
   }
 
   onDismiss(id) {
@@ -44,7 +49,9 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null;}
 
     return (
       <div className="page">
@@ -52,7 +59,7 @@ class App extends Component {
           <Search value={ searchTerm } onChange={ this.onSearchChange }>
             Search: 
           </Search>
-          <Table list={ list } pattern={ searchTerm } onDismiss={ this.onDismiss }/>
+          <Table list={ result.hits } pattern={ searchTerm } onDismiss={ this.onDismiss }/>
         </div>
       </div>
     );
